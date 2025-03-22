351978e1889e3ed662d3be7d4592a1df4a13027f
YASH2204V <yash.v220104@gmail.com>
2025-03-22 12:57:05 +0530

    #3

--- public/javascripts/external-script.js
+++ public/javascripts/external-script.js
@@ -81,3 +81,16 @@ if (filterToggle && filterSidebar && filterOverlay) {
         document.body.style.overflow = '';
     });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollPosition = localStorage.getItem("scrollPosition");
+    if (scrollPosition) {
+        window.scrollTo(0, parseInt(scrollPosition, 10));
+    }
+    const saveScrollPosition = () => {
+        localStorage.setItem("scrollPosition", window.scrollY);
+    };
+
+    window.addEventListener("beforeunload", saveScrollPosition);
+    return () => window.removeEventListener("beforeunload", saveScrollPosition);
+});
